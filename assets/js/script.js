10888b9fb6130d2743dc1f4411d7eb29df25414a
kimcc <15278020+kimcc@users.noreply.github.com>
2021-07-26 22:40:49 -0500

    ux enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,16 +53,18 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
-
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
-
+        $(event.target).addClass("dropoever-active");
     },
     out: function(event) {
-
+        $(event.target).removeClass("dropoever-active");
     },
     update: function(event) {
         var tempArr = [];
@@ -107,9 +109,11 @@ $("#trash").droppable({
     },
     over: function(event, ui) {
       console.log("over");
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event, ui) {
       console.log("out");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     }
 });
 
@@ -230,7 +234,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -265,6 +269,8 @@ var auditTask = function(taskEl) {
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
 
+  console.log(taskEl);
+
   // apply new class if task is near/over due date
   if (moment().isAfter(time)) {
     $(taskEl).addClass("list-group-item-danger");
@@ -283,7 +289,12 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function () {
+    $(".card .list-group-item").each(function(index, el) {
+      auditTask(el);
+    });
+}, (1000*60) * 30);
+
 // load tasks for the first time
 loadTasks();
-
 
